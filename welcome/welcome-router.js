743a3bca532e0877fa2bf277ba7ad2e2a4657568
Mohamed-Essamaali <Mohamed-Essamaali@lambdastudents.com>
2020-12-09 22:23:49 -0500

    added break line in displayed message

--- welcome/welcome-router.js
+++ welcome/welcome-router.js
@@ -4,7 +4,7 @@ const router = express.Router()
 
 router.get("/", (req, res) => {
 	res.status(200).json({
-		message: `Welcome ${process.env.COHORT}`,
+		message: `Welcome ${process.env.COHORT} \n`,
 		fact: process.env.FUN_FACT || "I Have no fun facts",
 	})
 })
